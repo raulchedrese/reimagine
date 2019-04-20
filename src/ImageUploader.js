aef2c43413bdeb7856dcfe6ff9945aef5898a287
Raul Chedrese <raulchedrese@gmail.com>
2019-04-19 20:29:20 -0600

    Implemented drag and drop.

--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -1,7 +1,8 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 export default function ImageUploader({ setImageSource }) {
   const fileInput = useRef(null);
+  const [isHovering, setIsHovering] = useState(false);
 
   const handleImageChange = () => {
     const selectedFile = fileInput.current.files[0];
@@ -10,10 +11,49 @@ export default function ImageUploader({ setImageSource }) {
     image.src = imageSource;
     setImageSource(imageSource);
   };
+
+  let classes = "drop-area";
+  if (isHovering) {
+    classes += " drop-area--hovering";
+  }
   return (
     <div>
-      <div>Upload Image</div>
-      <input ref={fileInput} onChange={handleImageChange} type="file" />
+      <div
+        className={classes}
+        onDrop={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          let dt = e.dataTransfer;
+          let files = dt.files;
+          const imageSource = window.URL.createObjectURL(files[0]);
+          const image = new Image();
+          image.src = imageSource;
+          setImageSource(imageSource);
+        }}
+        onDragEnter={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          setIsHovering(true);
+        }}
+        onDragOver={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          setIsHovering(true);
+        }}
+        onDragLeave={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          setIsHovering(false);
+        }}
+      >
+        <label htmlFor="uploadInput">Drop an image or click</label>
+        <input
+          id="uploadInput"
+          ref={fileInput}
+          onChange={handleImageChange}
+          type="file"
+        />
+      </div>
     </div>
   );
 }
